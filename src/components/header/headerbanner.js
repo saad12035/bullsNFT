3efdcd8a81b2bf86cc9f4cae7eecc0cc81d8dce1
Saad <saad12035@gmail.com>
2022-02-17 14:49:30 +0500

    first commit

---
+++ src/components/header/headerbanner.js
@@ -0,0 +1,60 @@
+import './headerbanner.css';
+import {Row, Col, Image} from 'antd';
+
+function HeaderBanner(){
+  return(
+      <div className="container">
+        <Row>
+          <Col span={4}>
+            <Image
+                style={{justifyContent:"center",width:80,height:80,marginLeft:100,marginTop:10}}
+                src={require('../../images/Untitled87_20220212095957.png')}
+            />
+          </Col>
+          <Col span={8} offset={7}>
+            <div className="header-menu">
+              <Row>
+                <Col span={6}>
+                  <h1>MINT</h1>
+                </Col>
+                <Col span={6}>
+                  <h1>ABOUT</h1>
+                </Col>
+                <Col span={6}>
+                  <h1>ROADMAP</h1>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col span={5}>
+            <Row>
+              <Col span={6}>
+                <Image
+                    style={{width:80,height:80}}
+                    src={require('../../images/pngegg.png')}
+                />
+
+              </Col>
+              <Col span={6}>
+                <Image
+                    style={{width:65,height:65,marginTop:8}}
+                    src={require('../../images/371907300_INSTAGRAM_ICON_1080.png')}
+                />
+              </Col>
+              <Col span={6}>
+                <Image
+                    style={{width:60,height:60,marginTop:8}}
+                    src={require('../../images/12.png')}
+                />
+              </Col>
+            </Row>
+
+
+
+          </Col>
+        </Row>
+      </div>
+  );
+}
+
+export default HeaderBanner;
---
+++ src/pages/homePage/homepage.js
@@ -0,0 +1,20 @@
+import { Layout } from 'antd';
+import HeaderBanner from "../../components/header/headerbanner";
+import './homepage.css';
+// import ContextBody from "../../components/contextbody/contextbody";
+// import FooterSite from "../../components/footersite/footersite";
+
+const { Header, Footer, Content } = Layout;
+function Home(){
+  return(
+      <Layout>
+        <div className="test">
+          <Header style={{ position: 'sticky', margin:'0', width: '100%',backgroundColor:"black" }}><HeaderBanner/></Header>
+          {/*<Content><ContextBody/></Content>*/}
+        </div>
+        {/*<Footer><FooterSite/></Footer>*/}
+      </Layout>
+  );
+}
+
+export default Home;
